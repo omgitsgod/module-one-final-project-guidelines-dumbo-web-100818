68c163e4983121de48ca45dd2bc335a45a9eec34
github username <github email address>
2018-10-22 18:55:10 -0400

    add rating

--- db/migrate/003_add_notes.rb
+++ db/migrate/003_add_notes.rb
@@ -3,8 +3,6 @@ class AddNotes < ActiveRecord::Migration[4.2]
     create_table :notes do |t|
       t.integer :user_id
       t.integer :subject_id
-      t.integer :rating
-      t.string :content
       t.boolean :bookmark
       t.timestamps
     end
