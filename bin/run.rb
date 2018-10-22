7232430636c90af23dcfece03b013bdc43f3ed45
github username <github email address>
2018-10-22 19:04:06 -0400

    update user method

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,5 @@
 require_relative '../config/environment'
 
-class User
-@@all = ["Brian", "Cory", "Ryan"]
-
-def self.all
-  @@all
-end
-end
 
 puts "---------------------------------------------"
 puts "-                                           -"
@@ -14,6 +7,11 @@ puts "-          Welcome to Class Journals        -"
 puts "-                                           -"
 puts "---------------------------------------------"
 
+# puts "1. Existing User"
+# puts "2. New User"
+#
+# option = gets.chomp
+# brian = User.create(name: "Brian", password: "123")
 puts "What is your name?"
 name = gets.chomp
 
@@ -24,13 +22,42 @@ def check_name(name)
     puts "Sorry, couldn't find your name. Please try again."
     name = gets.chomp
     user_object = User.all.find_by(name: name)
+
   end
+  puts "What is your password?"
+  password = gets.chomp
+  check_password(password, user_object)
 end
+
+def check_password(password, user_object)
+  pass = user_object.password == password
+  unless pass
+    puts "Sorry, wrong password. Please try again."
+    password = gets.chomp
+    pass = user_object.password == password
+  end
+  puts "Congratulations!"
+end
+
 check_name(name)
+
+# puts "What is your password?"
+# password = gets.chomp
+# def check_password(password, user_object)
+#   binding.pry
+#   pass = user_object.find_by(password: password)
+#   unless pass
+#     puts "Sorry, wrong password. Please try again."
+#     password = gets.chomp
+#     pass = user_object.find_by(password: password)
+#   end
+#   puts "Congratulations!"
+# end
+# check_password(password, holder)
 # check_name(name)
 
-puts "What is you password?"
-password = gets.chomp
+# puts "What is your password?"
+# password = gets.chomp
 
 binding.pry
 puts "hello"
