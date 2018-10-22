174491fee07593e1e6f13549a8bed74f8b040673
github username <github email address>
2018-10-22 17:37:29 -0400

    add run

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,36 @@
 require_relative '../config/environment'
 
+class User
+@@all = ["Brian", "Cory", "Ryan"]
 
+def self.all
+  @@all
+end
+end
 
-puts "HELLO WORLD"
+puts "---------------------------------------------"
+puts "-                                           -"
+puts "-          Welcome to Class Journals        -"
+puts "-                                           -"
+puts "---------------------------------------------"
+
+puts "What is your name?"
+name = gets.chomp
+
+def check_name(name)
+  user_object = User.all.find_by(name: name)
+  # condition = true
+  unless user_object
+    puts "Sorry, couldn't find your name. Please try again."
+    name = gets.chomp
+    user_object = User.all.find_by(name: name)
+  end
+end
+check_name(name)
+# check_name(name)
+
+puts "What is you password?"
+password = gets.chomp
+
+binding.pry
+puts "hello"
---
+++ lib/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+
+  def check_name(name)
+    user_object = User.all.find_by(name: name)
+    while false
+      puts "Sorry, couldn't find your name. Please try again."
+      name = gets.chomp
+      user_object = User.all.find_by(name: name)
+    end
+  end
+
+#   puts "What is your password #{name}?"
+#   check_password(password)
+#
+#
+#
+#
+# def check_password
+#   while false
+#     password = gets.chomp
+#     user_object.find_by(password: password)
+#     puts "Wrong password, try again?"
+#   end
+# end
+
+
+end
