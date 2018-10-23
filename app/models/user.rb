d8340781c31863fb93a1222b2739a528a727af43
github username <github email address>
2018-10-23 12:14:00 -0400

    add to user and create_note

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,38 @@ class User < ActiveRecord::Base
   belongs_to :group
   has_many :notes
   has_many :subjects, through: :notes
+
+  def create_a_note
+  puts "Choose from subjects"
+
+  self.subjects.each_with_index do |subject, index|
+    puts "#{index + 1}: #{subject.name}"
+  end
+
+  subjecttemp = gets.chomp
+
+  y = self.subjects.map do |x|
+    x.name
+  end
+
+  if y.include?(subjecttemp)
+    subject = self.subjects.find_by(name: subjecttemp)
+    puts "Rating?"
+    rating = gets.chomp
+    puts "Bookmark?"
+    puts "1. Yes"
+    puts "2. No"
+    bookmarktemp = gets.chomp
+      if bookmarktemp == 1
+        bookmark = true
+      elsif bookmarktemp == 2
+        bookmark = false
+      end
+      new_note = Note.create(user: self, subject: subject, rating: rating, bookmark: bookmark )
+      new_note << self.notes
+  else
+    puts "Please try again"
+      self.create_a_note
+    end
+  end
 end
--- bin/create_note.rb
+++ bin/create_note.rb
@@ -4,33 +4,4 @@ puts "-             Create A note                 -"
 puts "-                                           -"
 puts "---------------------------------------------"
 
-def create_a_note
-puts "Choose from subjects"
-
-self.subjects.each_with_index do |subject, index|
-  puts "#{index + 1}: #{subject.name}"
-end
-
-answer = gets.chomp
-
-  puts "Subject?"
-  subjecttemp = gets.chomp
-  subject = self.subjects.find_by(name: subjecttemp)
-  puts "Rating?"
-  rating = gets.chomp
-  puts "Bookmark?"
-  puts "1. Yes"
-  puts "2. No"
-  bookmarktemp = gets.chomp
-    if bookmarktemp == 1
-      bookmark = true
-    elsif bookmarktemp == 2
-      bookmark = false
-    end
-    new_note = Note.create(user: self, subject: subject, rating: rating, bookmark: bookmark )
-    new_note << self.notes
-else
-  puts "Please try again"
-    create_a_note
-  end
-end
+user_instance.create_a_note
