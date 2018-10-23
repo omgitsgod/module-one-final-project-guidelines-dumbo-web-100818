73a783ab972ee47a953d862e67f6c4ac10ad401e
github username <github email address>
2018-10-23 14:11:49 -0400

    update

--- lib/user.rb
+++ lib/user.rb
@@ -3,9 +3,6 @@ class User < ActiveRecord::Base
 has_many :notes
 belongs_to :groups
 has_many :subjects, through: :notes
-<<<<<<< HEAD
-=======
-
 
 def create_a_note
 puts "Choose from subjects"
@@ -20,11 +17,8 @@ subjecttemp = gets.chomp
 
 y = Subject.all.map do |x|
   x.name
->>>>>>> 77f0ef6b190be7367958d918a1aed10c9e75826e
 end
 
-<<<<<<< HEAD
-=======
 if y.include?(subjecttemp)
 subject = Subject.all.find_by(name: subjecttemp)
 subject = subject
@@ -47,44 +41,5 @@ else
     self.create_a_note
   end
 end
->>>>>>> 87a1a15e7165e8b53cc51f7826456f194eb5fa35
 
-# def create_a_note
-# puts "Choose from subjects"
-#
-# Subject.create(name: "math", id: 1)
-#
-# binding.pry
-# Subject.all.subjects.each_with_index do |subject, index|
-#  puts "#{index + 1}: #{subject.name}"
-# end
-#
-# subjecttemp = gets.chomp
-#
-# y = Subject.all.map do |x|
-#   x.name
-# end
-#
-# if y.include?(subjecttemp)
-# subject = self.subjects.find_by(name: subjecttemp)
-# subject = subjecttemp
-#   puts "Rating?"
-#   rating = gets.chomp
-#   puts "Bookmark?"
-#   puts "1. Yes"
-#   puts "2. No"
-#   bookmarktemp = gets.chomp
-#     if bookmarktemp == 1
-#       bookmark = true
-#     elsif bookmarktemp == 2
-#       bookmark = false
-#     end
-#     new_note = Note.create(user: self, subject: subject, rating: rating, bookmark: bookmark )
-#     new_note << self.notes
-# else
-#   puts "Please try again"
-#     self.create_a_note
-#   end
-# end
-#
-# end
+end
