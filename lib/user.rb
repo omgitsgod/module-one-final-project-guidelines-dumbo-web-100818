491bd59f00b7b164b4281d0732750b5f32338de8
github username <github email address>
2018-10-24 17:17:02 -0400

    finished?

--- lib/user.rb
+++ lib/user.rb
@@ -5,14 +5,17 @@ class User < ActiveRecord::Base
   has_many :subjects, through: :notes
 
   def create_a_note(user_instance)
+    arr = []
     puts `clear`
     create_banner
-    puts "Choose from subjects"
-    Subject.all.each_with_index do |subject, index|
-      puts "#{index + 1}: #{subject.name}"
+    # puts "Choose from subjects"
+    Subject.all.each do |subject|
+      arr << subject.name
+    # Subject.all.each_with_index do |subject, index|
+    #   puts "#{index + 1}: #{subject.name}"
     end
 
-    subjecttemp = gets.chomp
+    subjecttemp = $prompt.select("Choose your subject", arr)
 
     y = Subject.all.map do |x|
       x.name
@@ -40,64 +43,219 @@ class User < ActiveRecord::Base
   end
 
 #-View Notes-----------------------
+#
+#   def view_notes(user_instance)
+#     puts `clear`
+#     viewnotes_banner
+#     choose = $prompt.select("Choose an option", ["VIEW ALL", "VIEW BY SUBJECT", "VIEW BY RATING", "VIEW BY BOOKMARK", "RETURN TO HOMEPAGE"])
+#     if
+#       choose == "VIEW ALL"
+#       puts `clear`
+#       user_instance.notes.each do |note|
+#         puts "----------------------"
+#         puts "                      "
+#         puts note.content
+#         puts "                      "
+#         puts "----------------------"
+#       end
+#       $prompt.keypress("Press any key to return to previous page.")
+#       view_notes(user_instance)
+#
+#     elsif choose == "VIEW BY SUBJECT"
+#       puts `clear`
+#       `afplay sound_file/Connection-established-sound-effect.mp3`
+#       user_instance.subjects.select(:name).each_with_index do |subject,index|
+#         puts"      Subjects  #{index + 1}      "
+#         puts subject.name
+#         puts"--------------------------------------"
+#       end
+#       $prompt.keypress("Press any key to return to previous page.")
+#       view_notes(user_instance)
+#
+#     elsif
+#       choose == "VIEW BY RATING"
+#       puts `clear`
+#       `afplay sound_file/Connection-established-sound-effect.mp3`
+#       user_instance.notes.where(:rating => (1..10)).each_with_index do |note,index|
+#         puts"      Ranked notes  #{index + 1}      "
+#         puts note.content
+#         puts"--------------------------------------"
+#       end
+#       $prompt.keypress("Press any key to return to main menu.")
+#       view_notes(user_instance)
+#
+#     elsif
+#       choose == "VIEW BY BOOKMARK"
+#       puts `clear`
+#       `afplay sound_file/Connection-established-sound-effect.mp3`
+#       user_instance.notes.where(:bookmark => true).each_with_index do |note,index|
+#         puts"      Bookmarked notes  #{index + 1}      "
+#         puts note.content
+#         puts"-------------------------------------------"
+#         # user_instance.notes.where(:bookmark => true)
+#
+#       end
+#       view_notes(user_instance)
+#
+#     elsif
+#       choose == "RETURN TO HOMEPAGE"
+#       homepage(user_instance)
+#     end
+#
+#   end
+#
+# end
+#-View Notes-----------------------
+def view_notes(user_instance)
+  puts `clear`
+  `afplay sounds/Goat-sound-effect.mp3`
+  viewnotes_banner
+  yar = []
+user_instance.notes.each do |note|
+  binding.pry
+#  puts "----------------------"
+#  puts note.subject.name
+#  puts note.rating
+#  puts note.content
+#  puts "----------------------"
+yar << [note.subject.name, note.rating, note.content, note.bookmark]
+end
+table = TTY::Table.new ["Subject", "Rating", "Message", "Bookmark"], yar
 
-  def view_notes(user_instance)
-    puts `clear`
-    viewnotes_banner
-    choose = $prompt.select("Choose an option", ["VIEW ALL", "VIEW BY SUBJECT", "VIEW BY RATING", "VIEW BY BOOKMARK", "RETURN TO HOMEPAGE"])
-    if
-      choose == "VIEW ALL"
-      puts `clear`
-      user_instance.notes.each do |note|
-        puts "----------------------"
-        puts "                      "
-        puts note.content
-        puts "                      "
-        puts "----------------------"
-      end
-      $prompt.keypress("Press any key to return to previous page.")
-      view_notes(user_instance)
+puts table.render(:ascii)
+  #puts"(1). View by Subject"
+  #puts"(2). View by Rating"
+  #puts"(3). View by Bookmark"
+  option = $prompt.select("What now?: ", ["Edit", "Sort by Subject", "Sort by Rating", "Sort by Bookmark", "Main Menu"])
 
-    elsif choose == "VIEW BY SUBJECT"
-      puts `clear`
-      `afplay sound_file/Connection-established-sound-effect.mp3`
-      user_instance.subjects.select(:name).each_with_index do |subject,index|
-        puts"      Subjects  #{index + 1}      "
-        puts subject.name
-        puts"--------------------------------------"
-      end
-      $prompt.keypress("Press any key to return to previous page.")
-      view_notes(user_instance)
+  if option == "Sort by Subject"
+    arr = []
+    Subject.all.each do |subject|
+      arr << subject.name
+    end
 
-    elsif
-      choose == "VIEW BY RATING"
-      puts `clear`
-      `afplay sound_file/Connection-established-sound-effect.mp3`
-      user_instance.notes.where(:rating => (1..10)).each_with_index do |note,index|
-        puts"      Ranked notes  #{index + 1}      "
-        puts note.content
-        puts"--------------------------------------"
-      end
-      $prompt.keypress("Press any key to return to main menu.")
-      view_notes(user_instance)
+    subjecttemp = $prompt.select("Choose your subject", arr)
 
-    elsif
-      choose == "VIEW BY BOOKMARK"
-      puts `clear`
-      `afplay sound_file/Connection-established-sound-effect.mp3`
-      user_instance.notes.where(:bookmark => true).each_with_index do |note,index|
-        puts"      Bookmarked notes  #{index + 1}      "
-        puts note.content
-        puts"-------------------------------------------"
-        # user_instance.notes.where(:bookmark => true)
+    y = Subject.all.map do |x|
+      x.name
+    end
 
-      end
-      view_notes(user_instance)
+    if y.include?(subjecttemp)
+    subject = Subject.all.find_by(name: subjecttemp)
+  end
+  ##############################
 
-    elsif
-      choose == "RETURN TO HOMEPAGE"
+
+  hmm = []
+  blooblooo = user_instance.notes.where(subject: subject)
+  blooblooo.each do |x|
+    hmm << [x.subject.name, x.rating, x.content, x.bookmark]
+  end
+  table2 = TTY::Table.new ["Subject", "Rating", "Message", "Bookmark"], hmm
+  puts table2.render(:ascii)
+  subchoice = $prompt.select("Where to? : ", ["Edit", "Main Menu"])
+
+  ###########################
+  if subchoice == "Edit"
+    edit_note(user_instance)
+    elsif subchoice == "Main Menu"
+    homepage(user_instance)
+  end
+  ########################
+  elsif
+    option == "Sort by Rating"
+    rating = $prompt.slider("Rating?", max:10, step: 1)
+    x = user_instance.notes.where(rating: rating)
+
+    hmm2 = []
+    x.each do |note|
+      hmm2 << [note.subject.name, note.rating, note.content, note.bookmark]
+    end
+
+    table3 = TTY::Table.new ["Subject", "Rating", "Message", "Bookmark"], hmm2
+    puts table3.render(:ascii)
+    subchoice = $prompt.select("Where to? : ", ["Edit", "Main Menu"])
+    ###########################
+    if subchoice == "Edit"
+      edit_note(user_instance)
+      elsif subchoice == "Main Menu"
       homepage(user_instance)
     end
+    ########################
+  elsif
+    option == "Main Menu"
+    homepage(user_instance)
+
+  elsif
+    option == "Edit"
+    edit_note(user_instance)
+
+  elsif
+    option == "Sort by Bookmark"
+    bookmark = $prompt.yes?("Is your note bookmarked?")
+    x = user_instance.notes.where(bookmark: bookmark)
+
+    hmm3 = []
+    x.each do |note|
+      hmm3 << [note.subject.name, note.rating, note.content, note.bookmark]
+    end
+
+    table4 = TTY::Table.new ["Subject", "Rating", "Message", "Bookmark"], hmm3
+    puts table4.render(:ascii)
+    subchoice = $prompt.select("Where to? : ", ["Edit", "Main Menu"])
+    ###########################
+    if subchoice == "Edit"
+      edit_note(user_instance)
+      elsif subchoice == "Main Menu"
+      homepage(user_instance)
+    end
+    ########################
+   end
+ end
+
+  def edit_note(user_instance)
+    arr = []
+    user_instance.notes.each do |note|
+      arr << ["#{note.id} #{note.created_at}  #{note.subject.name}  #{note.rating}  #{note.content}"]
+    end
+
+    notetemp = $prompt.select("Choose your note: ", arr)
+    idnum = notetemp.split[0].to_i
+
+    lala = user_instance.notes.find(idnum)
+
+    editchoice = $prompt.select("What now?: ", ["Edit Rating", "Edit Content", "Delete", "Main Menu"])
+
+    ##########
+    if editchoice == "Edit Rating"
+      rat1 = $prompt.ask("Enter new rating (NUMBER): ")
+      lala.rating = rat1
+      lala.save
+      user_instance.notes.reload
+      $prompt.keypress("SAVED! Press any key to return to your notes.")
+      view_notes(user_instance)
+    elsif editchoice == "Edit Content"
+      con1 = $prompt.ask("Enter new content: ")
+      lala.content = con1
+      lala.save
+      user_instance.notes.reload
+      $prompt.keypress("SAVED! Press any key to return to your notes.")
+      view_notes(user_instance)
+    elsif editchoice == "Delete"
+      yahh = $prompt.yes?("ARE YOU SURE?")
+      if yahh
+        lala.destroy
+        user_instance.notes.reload
+        $prompt.keypress("DESTROYED! Press any key to return to your notes.")
+        view_notes(user_instance)
+      end
+
+    elsif editchoice == "Main Menu"
+      homepage(user_instance)
+    end
+    ##########
+    binding.pry
+
 
   end
 
